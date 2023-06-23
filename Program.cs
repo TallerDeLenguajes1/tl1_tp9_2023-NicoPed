0081f563feec9b0524900ec7b681eed7f8a3a7c8
NicoPed <nikopedraza10@gmail.com>
2023-06-23 14:44:16 -0300

    TerminadoPunto1

--- Program.cs
+++ Program.cs
@@ -1,15 +1,44 @@
 ﻿using ClaseBitCoins;
+using System.Net;
+using System.Text.Json;
 internal class Program
 {
     private static void Main(string[] args)
     {
         PrecioBitcoin obtenerBTC;
         obtenerBTC = ConsumirAPI();
-
+        Console.WriteLine (@"╔═══════════════════════════╗  ╔═══════════════════════════╗");
+        Console.WriteLine (@$"║ {obtenerBTC.bpi.USD.description.PadRight(26)}║  ║ {obtenerBTC.bpi.EUR.description.PadRight(26)}║");
+        Console.WriteLine (@"╠───────────────────────────╣  ╠───────────────────────────╣");
+        Console.WriteLine (@$"║ {obtenerBTC.bpi.USD.rate.PadRight(26)}║  ║ {obtenerBTC.bpi.EUR.rate.PadRight(26)}║");
+        Console.WriteLine (@"╚═══════════════════════════╝  ╚═══════════════════════════╝");
+        Console.WriteLine (@"               ╔═══════════════════════════╗");
+        Console.WriteLine (@$"               ║ {obtenerBTC.bpi.GBP.description.PadRight(26)}║");
+        Console.WriteLine (@"               ╠───────────────────────────╣");
+        Console.WriteLine (@$"               ║ {obtenerBTC.bpi.GBP.rate.PadRight(26)}║");
+        Console.WriteLine (@"               ╚═══════════════════════════╝");
+   
     }
-    PrecioBitcoin ConsumirAPI(){
-        
-        var hola = new PrecioBitcoin();
-        return hola;
+    public static PrecioBitcoin ConsumirAPI(){
+        var url = $"https://api.coindesk.com/v1/bpi/currentprice.json";
+        var request = (HttpWebRequest)WebRequest.Create(url);
+
+        request.Method = "GET";
+        request.ContentType = "application/json";
+        request.Accept = "application/json";
+        PrecioBitcoin obtBTC = null;
+        using (WebResponse response = request.GetResponse())
+        {
+            using (Stream strReader = response.GetResponseStream())
+            {
+                if (strReader == null) return obtBTC;
+                    using (StreamReader objReader = new StreamReader(strReader))
+                    {
+                        string responseBody = objReader.ReadToEnd();
+                        obtBTC = JsonSerializer.Deserialize<PrecioBitcoin>(responseBody);// como es un objeto y no una lista no se pone list
+                    }
+            }
+        }
+        return obtBTC;
     }
 }
