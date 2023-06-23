6cbe4d1732659461c713e693e6bcc9bf5fefece1
NicoPed <nikopedraza10@gmail.com>
2023-06-23 12:55:17 -0300

    Initial commit con la clase bitcoins ya creada

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using ClaseBitCoins;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrecioBitcoin obtenerBTC;
+        obtenerBTC = ConsumirAPI();
+
+    }
+    PrecioBitcoin ConsumirAPI(){
+        
+        var hola = new PrecioBitcoin();
+        return hola;
+    }
+}
